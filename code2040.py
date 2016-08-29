49f47b7c6901dd299537354f618c35c8808a0449
klipspringr <mari.tir3sias@gmail.com>
2016-08-29 15:46:42 -0700

    Add Step 5

--- code2040.py
+++ code2040.py
@@ -5,20 +5,28 @@ sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 
 # module that makes HTTP requests easy!
 import requests
+import json
 
-new_req = requests.post("http://challenge.code2040.org/api/prefix", data={'token': 'abfa1dcb4c71b9fd3f790440b332f76c'})
-req = new_req.json()
+# module that will help with converting ISO 8601 to a Python datetime object
+import dateutil.parser
+import datetime
 
-prefix = req['prefix']
-array = req['array']
-array_wo_prefix = []
+req = requests.post("http://challenge.code2040.org/api/dating", data={'token': 'abfa1dcb4c71b9fd3f790440b332f76c'}).json()
 
-# only add strings that don't have the the prefix to array_wo_prefix
-for i in range(len(array)):
-	if array[i][:len(prefix)] != prefix:
-		array_wo_prefix.append(str(array[i]))
+# grab the ISO 6801 datestamp and convert to python datetime object
+old_time = dateutil.parser.parse(req['datestamp'])
 
-payload = {'token': 'abfa1dcb4c71b9fd3f790440b332f76c', 'array': array_wo_prefix}
+# add the interval to the initial datestamp, and create a new one
+new_time = old_time + datetime.timedelta(seconds=req['interval'])
+
+# the format wanted by the API is somewhat ambiguous. though a valid ISO 6801 may end in either "+00:00" or 
+# "Z", the API will only accept a string ending in "Z". so, if the string does not end with "Z", replace it.
+if new_time.isoformat()[-6:] == "+00:00":
+	new_time = new_time.isoformat()[:-6] + 'Z'
+else:
+	new_time = new_time.isoformat()
+
+payload = {'token': 'abfa1dcb4c71b9fd3f790440b332f76c', 'datestamp':new_time}
 
 # ensure that the content type of the payload is JSON, otherwise it will not be sent as a dictionary!!
-check = requests.post("http://challenge.code2040.org/api/prefix/validate", json=payload)
+check = requests.post("http://challenge.code2040.org/api/dating/validate", json=payload)
